7ba88ecfc8e6e51ebdc1200442581e5743f584e4
Eduardo <eduardooffside@gmail.com>
2021-05-15 23:58:16 -0300

    update footer

--- calculator/src/footer/Footer.jsx
+++ calculator/src/footer/Footer.jsx
@@ -2,12 +2,14 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light"> App desenvolvido por Eduardo Silva</p>
-                <p className="text-light">
-                    GitHub: <a href="https://github.com/dududueedu" target="_blank" rel="noreferrer"> dududueedu </a>
-                </p> 
+                <p className="text-light"> App desenvolvido por Eduardo Silva
+                <br/><strong> Contatos: </strong><br/>
+                </p>
+                <span className="text-light">
+                    GitHub: <a className="text-white" href="https://github.com/dududueedu" target="_blank" rel="noreferrer"> dududueedu </a>
+                </span> 
                 <p className="text-light"><small>
-                    Instagram: <a href="https://instagram.com/du.dudu.e.edu.__" target="_blank" rel="noreferrer"> 
+                    Instagram: <a className="text-white" href="https://instagram.com/du.dudu.e.edu.__" target="_blank" rel="noreferrer"> 
                     @du.dudu.e.edu.__ </a></small>
                 </p>
             </div>
