47b724f96b9c888f3ba7183585ad32b6f720a715
Eduardo <eduardooffside@gmail.com>
2021-05-20 17:35:33 -0300

    logo-position

--- calculator/src/components/navBar/Nav.jsx
+++ calculator/src/components/navBar/Nav.jsx
@@ -4,8 +4,8 @@ function Nav() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-logo">
-                <a className="navbar-brand" href="/">
-                    <img src={logo} alt="" width="40" height="40" />
+                <a href="/">
+                    <img src={logo} alt="" width="38" height="38" />
                 </a>
             </div>
         </nav>
