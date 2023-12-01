57b7274fe1245777b45ecc5c34cf84295ae8508a
Eduardo Gomes <eduardooffside@gmail.com>
2023-12-01 09:36:52 -0300

    remove bugs

--- calculator/src/functions/showdata.js
+++ calculator/src/functions/showdata.js
@@ -1,56 +1,54 @@
 export const add1=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 1;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 1;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 1;
-
-        console.log("teste")
 }
 export const add2=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 2;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 2;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 2;
 }
 export const add3=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 3;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 3;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 3;
 }
 export const add4=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 4;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 4;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 4;
 }
 export const add5=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 5;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 5;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 5;
 }
 export const add6=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 6;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 6;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 6;
 }
 export const add7=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 7;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 7;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 7;
 }
 export const add8=() =>{
+    if (document.getElementById("txt1").value && !document.getElementById("text2"))
+        document.getElementById("txt2").value = 8;
     if (!document.getElementById("txt1").value)
         document.getElementById("txt1").value = 8;
-    if (document.getElementById("txt1").value)
-        document.getElementById("txt2").value = 8;
 }
 export const add9=() =>{
-    if (!document.getElementById("txt1").value)
-        document.getElementById("txt1").value = 9;
-    if (document.getElementById("txt1").value)
+    if(document.getElementById("txt1").value && !document.getElementById("text2"))
         document.getElementById("txt2").value = 9;
+    if (!document.getElementById("txt1").value)
+        document.getElementById("txt1").value = 9;    
 }
